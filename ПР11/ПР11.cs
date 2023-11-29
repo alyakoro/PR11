7804f11b9c15738f5a950615af8de6ec6ae9998f
alyak <alya.korobeynikova99@gmail.com>
2023-11-29 14:32:02 +0300

    Добавьте файлы проекта.

---
+++ ПР11/ПР11.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ПР11
+{
+    public struct Work
+    {
+        public int kod; //код сотрудника
+        public double zarabotok; //заработок сотрудника
+        public string name; //имя сотрудника
+        public string surname; //фамилия сотрудника 
+    }
+
+
+    class ПР11
+    {
+        enum Name { Иван, Петр, Ирина, Ольга, Ян, Света }
+        enum Surname { Сидоров, Андреев, Иванова, Выходова, Юрин, Абрамович }
+        enum Telofone { тел3453212, тел453212, тел4532312, тел444, тел3421232, тел3432124}
+        private static Type t1 = typeof(Name);
+        private static Type t2 = typeof(Surname);
+        private static Type t3 = typeof(Telofone);
+
+        static void Main(string[] args)
+        {
+            int k = 0;
+            Work[] w = new Work[6];
+            Name[] names;
+            names = Enum.GetValues(typeof(Name)).Cast<Name>().ToArray();
+            Surname[] surnames;
+            surnames = Enum.GetValues(typeof(Surname)).Cast<Surname>().ToArray();
+            Telofone[] telofones;
+            telofones = Enum.GetValues(typeof(Telofone)).Cast<Telofone>().ToArray();
+
+            Random r = new Random();
+            List<string> surname_A = new List<string>();
+            String reg = @"...\d{7}";
+            String reg_2 = @"[А]";
+            
+            for (int i = 0; i < w.Length; i++)
+            {
+                //сравнение телефона определенного сотрудника
+                //с регулярным выражением reg ( 3 буквы и 7 цифр )
+                string tel = "\nТелефон не верный: ";
+                if (Regex.IsMatch(Convert.ToString(telofones[i]), reg)) tel = "\nТелефон: ";
+
+                w[i].kod = i;
+                w[i].zarabotok = r.Next(16000, 30000);
+
+                //Сравнение заработной платы с прожиточным минимумом и замена текста
+                string zar = "\nЗаработная плата: ";
+                if (w[i].zarabotok < 20000) zar = "\nЗаработная плата меньше " +
+                        "прожиточного минимума: ";
+
+                //Вывод всей информации
+                Console.WriteLine("\nКод сотрудника № " + (i + 1) + 
+                    "\nИмя: " + names[i] + 
+                    "\nФамилия: " + surnames[i] + 
+                     zar + w[i].zarabotok + 
+                     tel + telofones[i]);
+
+                //Сравненеи фамилии определенного сотрудника
+                //с регулярным выражением reg_2 (Начало фамилии на А)
+                if (Regex.IsMatch(Convert.ToString(surnames[i]), reg_2)) 
+                    surname_A.Add(Convert.ToString(surnames[i]));
+            }
+            //Вывод фамилий, что начинаются на А
+            foreach (string s in surname_A) Console.WriteLine("\nФамилии на 'А': " + s);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ПР11_2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР11_2
+{
+    public struct Work
+    {
+        public string work; //поля фамилии и имя
+        public string dolgost; //полу должности
+        public uint oklad; //поле оклада
+        public string kod; //поле код
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            ArrayList work = new ArrayList();
+            work.AddRange(new string[] { "Иван Сидоров", "Петр Андреев", "Ирина Иванова", "Ольга Выходова", "Ян Юрин" });
+            ArrayList dolgost = new ArrayList();
+            dolgost.AddRange(new string[] { "повар", "кондитер", "пекарь", "тестомес", "кондитер" });
+            ArrayList oklad = new ArrayList();
+            oklad.AddRange(new uint[] { 25000, 12464, 35654, 34231, 20453 });
+            ArrayList kod = new ArrayList();
+            kod.AddRange(new string[] { "СИ001", "АП002", "ИИ003", "ВО004", "ЮЯ005" });
+
+            //
+            Console.Write("Текущее содержание списка сотрудников: \n\n");
+            for (int i = 0; i < work.Count; i++) Console.Write(kod[i] + " " +
+                work[i] + " " + dolgost[i] + " " + oklad[i] + " руб/месяц\n");
+            //
+            Console.WriteLine("\nСписок сотрудников по алфавиту");
+            foreach (object w in work) Console.WriteLine(w);
+
+            string ex = "";
+            while(ex != "exit")
+            {
+                Console.WriteLine("\n1. Добавить сотрудника \n2. Уволить сотрудника:");
+                int kk = Convert.ToInt16(Console.ReadLine());
+                if (kk == 1)
+                {
+                    Console.Write("Введите имя и фамилию сотрудника: ");
+                    work.Add(Console.ReadLine());
+                    Console.Write("Введите должность сотрудника: ");
+                    dolgost.Add(Console.ReadLine());
+                    Console.Write("Введите оклад сотрудника: ");
+                    oklad.Add(Console.ReadLine());
+                    Console.Write("Введите код сотрудника: ");
+                    kod.Add(Console.ReadLine());
+
+                }
+                else if (kk == 2)
+                {
+                    int un = 0;
+                    Console.WriteLine("\nВведите код уволенного сотрудника: ");
+                    string k = Console.ReadLine();
+                    for (int i = 0; i < kod.Count; i++)
+                    {
+                        if (k == Convert.ToString(kod[i]))
+                        {
+                            un = kod.IndexOf(k);
+                            Console.WriteLine("\nСотрудник уволен: " + work[un] + " " + kod[un] + "\n");
+                            kod.RemoveAt(un);
+                            work.RemoveAt(un);
+                            dolgost.RemoveAt(un);
+                            oklad.RemoveAt(un);
+                        }
+                    }
+                }
+                else Console.WriteLine("\nВведите повторно!");
+            
+
+            Console.Write("\nСодержимое с изменениями\n");
+            for (int j = 0; j < kod.Count; j++) Console.WriteLine(kod[j] + " " + work[j] +
+                " " + dolgost[j] + " " + oklad[j] + " руб/месяц");
+            Console.Write("\nДля выхода, введите exit: ");
+            ex = Console.ReadLine();
+            }
+        }
+    }
+}
